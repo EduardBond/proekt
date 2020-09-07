f254e0e98615b975dbd5dd600025ae410f24ffa8
Eduard <bondarev.edik@inbox.ru>
2020-09-07 18:14:12 +0500

    df

---
+++ znak/znak/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace znak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool f = false;
+            FileStream f1 = new FileStream("D:\\znak.txt", FileMode.Open);
+            StreamReader rd = new StreamReader(f1);
+            FileStream f2 = new FileStream("D:\\znak2.txt", FileMode.Create);
+            StreamWriter wr = new StreamWriter(f2);
+            string rds;
+            while (!rd.EndOfStream)
+            {
+                rds = rd.ReadLine();
+                for (int i = 0; i < rds.Length; i++)
+                {
+                    if (rds[i] == ' ' && f == false)
+                    {
+                        wr.Write("? ");
+                        f = true;
+                    }
+                    else wr.Write(rds[i]);
+                }
+                f = false;
+                wr.WriteLine();
+
+            }
+            
+            Console.ReadKey(true);
+            rd.Close();
+            f1.Close();
+            wr.Close();
+            f2.Close();
+        }
+    }
+}
